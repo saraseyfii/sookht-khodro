7b8c0b76e02153f236d9e5a4a063a29e3626bc53
saraseyfii <seyfisara68@gmail.com>
2024-06-13 13:10:43 +0330

    Add files via upload

---
+++ Sukht.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+{
+	float tank fuel, fuel Rate, distance;
+	cin >> tankfuel >> fuel Rate;
+	distance - (tankfuel / fuelRate) * 100;
+	if (distance < 100) {
+		cout << "please refuel tank";
+	}
+	cout << distance;
+	return 0;
+}
